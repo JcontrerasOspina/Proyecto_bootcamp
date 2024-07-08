9bdef67c3f15c3485a546329aebd8a7bc9c502a9
Jimena <jimenaospina28@gmail.com>
2024-07-08 09:46:57 -0500

    Se crean los eventos de los input en el archivo script.js

--- js/script.js
+++ js/script.js
@@ -7,4 +7,9 @@ const datos = {
     pedido: ''
 }
 
-
+/* eventos de los input */
+let nombre = document.getElementById('#nombre');
+let correo = document.getElementById('#correo');
+let telefono = document.getElementById('#telefono');
+let direccion = document.getElementById('#direccion');
+let pedido = document.getElementById('#pedido');
