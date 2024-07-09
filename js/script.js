319463213a7eb6b6bec26dd252d0dc98c0da1193
Jimena <jimenaospina28@gmail.com>
2024-07-08 20:58:32 -0500

    se añade la funcion leeTexto en js

--- js/script.js
+++ js/script.js
@@ -13,3 +13,13 @@ let correo = document.getElementById('#correo');
 let telefono = document.getElementById('#telefono');
 let direccion = document.getElementById('#direccion');
 let pedido = document.getElementById('#pedido');
+
+nombre.addEventListener('input', leerTexto);
+correo.addEventListener('input', leerTexto);
+telefono.addEventListener('input', leerTexto);
+direccion.addEventListener('input', leerTexto);
+pedido.addEventListener('input', leerTexto);
+
+function leerTexto(evento){
+    datos[evento.target.id] = evento.target.value;
+}
